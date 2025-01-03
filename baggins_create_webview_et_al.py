4950b521b843e09d57fd0c811354ded961b15bd5
HariZalan <harizalan12@gmail.com>
2025-01-03 06:20:59 +0100

    Cleanup

--- baggins_create_webview_et_al.py
+++ baggins_create_webview_et_al.py
@@ -9,8 +9,6 @@ gi.require_version("Gtk","4.0")
 gi.require_version("WebKit","6.0")
 from gi.repository import Gtk, Gdk, Gio, GLib
 from gi.repository import WebKit as WebKit2
-#import faulthandler
-#faulthandler.enable()
 def openWebPage(page=None,traditional=False,name="Baggins",version="2.2",mainpage=None,private=False,kiosk=False,title=None,autoclosable=False,boxonly=False,search_engine="https://duckduckgo.com/?q=",aid=None,tabbed=False,vertabbed=True,applicationn=None):
 	if (applicationn==None):
 		application=Gtk.Application(application_id=aid or "org.freedesktop.Baggins",flags=Gio.ApplicationFlags.ALLOW_REPLACEMENT)
@@ -38,11 +36,9 @@ def openWebPage(page=None,traditional=False,name="Baggins",version="2.2",mainpag
 				nb.show()
 				nb.set_current_page(-1)
 				tab=nb.get_nth_page(-1)
-				#nb.hide()
 				nb.set_tab_reorderable(tab,True)
 				nb.set_show_border(False)
 				setshowtabs(nb)
-				#nb.set_tab_label(tab,Gtk.Label(label=box.title))
 			nb.append_page(box)
 			if (vertabbed):
 				nb.set_tab_pos(Gtk.PositionType.LEFT)
@@ -90,7 +86,6 @@ def openWebPage(page=None,traditional=False,name="Baggins",version="2.2",mainpag
 						nb.prev_page()
 			b2.connect("clicked",closetab)
 			hb=Gtk.HeaderBar()
-			#hb.set_show_close_button(True)
 			hb.pack_start(b)
 			hb.pack_start(b2)
 			window.set_titlebar(hb)
@@ -104,7 +99,6 @@ def openWebPage(page=None,traditional=False,name="Baggins",version="2.2",mainpag
 		application.run(None)
 	else:
 		activate(application, tabbed)
-#from bagheader import dialogdisplay
 def openWebPage2(page=None,traditional=False,webv=None,name="Baggins",version="2.2",mainpage=None,private=False,kiosk=False,title=None,autoclosable=False,boxonly=False,search_engine="https://duckduckgo.com/?q=",aid="org.freedesktop.Baggins",parent=None,application=None,webvkeypress=None):
 	if (aid==None):
 		aid="org.freedesktop.Baggins"
@@ -150,12 +144,8 @@ def openWebPage2(page=None,traditional=False,webv=None,name="Baggins",version="2
 			if (entry.get_text()!="about:home"):
 				url=entry.get_text()
 				paersar=urllib.parse.urlparse(url)
-				#paersar2=urllib.parse.urlparse("http://"+url)
-				
-				if (paersar.scheme):# and re.fullmatch("[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]*)"
+				if (paersar.scheme):
 					webv.load_uri(url)
-				#elif (paersar2.scheme):
-				#	webv.load_uri("http://"+url)
 				else:
 					searchuri(entry,webv)
 			else:
@@ -206,21 +196,15 @@ def openWebPage2(page=None,traditional=False,webv=None,name="Baggins",version="2
 						url=webv.get_uri()
 						GLib.idle_add(lambda: entry.set_text("about:home"))
 				if (webv.is_loading() and reload!=None):
-					#GLib.idle_add(reload.set_label,"🗙")
-					#GLib.idle_add(reload.connect,"clicked",lambda x: webv.stop_loading())
 					GLib.idle_add(lambda: reload.set_sensitive(False))
 				else:
 					GLib.idle_add(lambda: reload.set_sensitive(True))
-				#else:
-				#	GLib.idle_add(reload.set_label,"⟳")
-				#	GLib.idle_add(reload.connect,"clicked",lambda x: webv.reload())
 	def displayuri(attercop,hittestresult,oldtomnoddy,TheThirdOne,traditional):
 		if (hittestresult.context_is_link()==True):
 			TheThirdOne.set_visible(True)
 			TheThirdOne.set_text(WebKit2.uri_for_display(hittestresult.get_link_uri()))
 		else:
 			TheThirdOne.set_text("")
-			#if (traditional==True):
 	def goback(webv):
 		def thread():
 			while webv.is_loading():
@@ -235,10 +219,7 @@ def openWebPage2(page=None,traditional=False,webv=None,name="Baggins",version="2
 			webv.go_forward()
 		ourThread=threading.Thread(target=thread,daemon=True)
 		ourThread.start()
-	#window.set_icon_name("gnome-nettool")
 	icon=bilbospath+"/Bilbo.png"
-	#window.set_icon("/".join(os.path.realpath(__file__).split("/")[:-1])+"/Bilbo.png")
-	#icon=Gtk.IconTheme.load_icon(Gtk.IconTheme(),"3x3",Gtk.IconLookupFlags.USE_BUILTIN,None)
 	if (webv==None):
 		def loadfailed(webv,uri,cert,err):
 			if (webv.can_go_back()==True):
@@ -293,20 +274,17 @@ def openWebPage2(page=None,traditional=False,webv=None,name="Baggins",version="2
 		webv.connect("create",lambda x,y: openinnewwindow(x,y,kiosk,traditional,private,title,application))
 		webv.connect("mouse-target-changed",lambda x,y,z: displayuri(x,y,z,The_third_one,traditional))
 		webv.connect("load-failed-with-tls-errors",loadfailed)
-		#WebKit2.Download().connect("decide-destination",downloadNotify)
 		if (private==False):
 			webv.cookieManager=WebKit2.NetworkSession.get_default().get_cookie_manager()
 			WebKit2.CookieManager.set_persistent_storage(webv.cookieManager,bagpath+"/.baggins.storage",WebKit2.CookiePersistentStorage(WebKit2.CookiePersistentStorage.TEXT))
 		settings=webv.get_settings()
 		WebKit2.Settings.set_user_agent_with_application_details(settings,name,version)
-		#WebKit2.CookieManager.set_persistent_storage("baggins.storage")
 		webv.load_uri(page)
 		WebKit2.Settings.set_enable_webrtc(settings,True)
 		WebKit2.Settings.set_enable_media_stream(settings,True)
 		WebKit2.Settings.set_enable_developer_extras(settings,True)
 		WebKit2.Settings.set_enable_back_forward_navigation_gestures(settings,True)
 		WebKit2.Settings.set_default_charset(settings,"utf-8")
-		#WebKit2.Settings.set_enable_caret_browsing(settings,True)
 		WebKit2.Settings.set_javascript_can_access_clipboard(settings,True)
 		def terminated(hight_reason):
 			webv.load_alternate_html("""The web process has terminated unexpectedly<p><i>Clap! Snap! the black crack!
@@ -331,7 +309,6 @@ Round and round far underground<br/>
 				box=Gtk.Box()
 				window.set_child(box)
 				label=Gtk.Label.new("The site wants to request a permission.")
-				#The_third_one=Gtk.Label()
 				button1=Gtk.Button(label="Permit")
 				button2=Gtk.Button(label="Deny")
 				button1.connect("clicked", lambda x: permit())
@@ -350,13 +327,8 @@ Round and round far underground<br/>
 					result=True
 					
 				return result
-		#pm=WebKit2.UserMediaPermissionRequest
-		#print(type(pm))
-		#pm.notify()
 		webv.connect("permission-request", lambda x,y: cameraandmicrophone(application))
 		webv.connect("web-process-terminated",lambda x,y: terminated(x))
-		#if (private==True):
-		#	pass#WebKit2.Settings.set_enable_private_browsing(settings,True) deprecated
 	if (kiosk==False):
 		box2=Gtk.Box()
 		entrie=Gtk.Entry()
@@ -374,8 +346,6 @@ Round and round far underground<br/>
 		button5.connect("clicked",lambda x: webv.reload())
 		button6=Gtk.Button.new_from_icon_name("folder-download-symbolic")
 		button6.connect("clicked",lambda x: webv.save_to_file(Gio.File.new_for_path(os.path.expanduser("~")+"/Downloads/"+str(random.randrange(10000))+".mhtml"),WebKit2.SaveMode(0),None,None,None))
-		#button6=Gtk.Button(label="Inspect")
-		#button6.connect("clicked",lambda x: webv.get_inspector().show())
 		box2.append(button)
 		box2.append(button2)
 		box2.append(button5)
@@ -383,11 +353,6 @@ Round and round far underground<br/>
 		box2.append(button0)
 		box2.append(button4)
 		box2.append(button6)
-	#def keypressed(wget,eventitself):
-	#	return False
-	#box2.append(button6)
-	#window.connect("key-press-event",keypressed)
-	#window.add_accelerator(button2,"<Control>d","clicked")
 	box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 	The_third_one=Gtk.Label()
 	if (traditional==True):
@@ -395,47 +360,27 @@ Round and round far underground<br/>
 		if (kiosk==False):
 			box.prepend(box2)
 		box.prepend(The_third_one)
-		#if (traditional==True):
-		#	The_third_one.set_visible(False)
 	else:
-		#if The_third_one.get_parent():
-		#	print("ach")
-		#	The_third_one.get_parent().remove(The_third_one)
 		box.prepend(The_third_one)
 		if (kiosk==False):
 			box.append(box2)
-		#if (traditional==True):
-		#	The_third_one.set_visible(False)
 		webvbox=Gtk.Box()
 		webvbox.prepend(webv)
 		box.prepend(webvbox)
 	if(box!=None):
 		box.title=webv.get_title()
-		#if (kiosk==True and spinner==True):
-		#	box.prepend(spinnerr,fill=True,expand=False,padding=0)
-	#nb=Gtk.Notebook()
-	#nb.new()
-	#nb.append_page(box)
-	#window.add(nb)
 	if (kiosk==False):
 		urlthread=threading.Thread(target=ourthread,args=(entrie,webv,autoclosable,button,button2,button5,box,),daemon=True)
 	else:
 		urlthread=threading.Thread(target=ourthread,args=(None,webv,autoclosable,),daemon=True)
 	urlthread.start()
-	#box.set_parent(parent) if parent else False
 	webv.set_focusable(False)
 	box.webv=webv
 	box.goback=goback
 	box.goforward=goforward
 	box.reload=webv.reload
 	return box
-#if (kiosk==True and spinner==True):
-#	spinnerr=Gtk.Spinner()
-#	spinnerr.set_visible(False)
 def openinnewwindow(wv,navact,kiosk,traditional,private,title,application):
 	x=navact.get_request().get_uri()
 	openWebPage(page=x,kiosk=kiosk,traditional=traditional,private=private,title=title,applicationn=application)
 	return None
-#if (kiosk==True and spinner==True):
-#	spinnerr=Gtk.Spinner()
-#	spinnerr.set_visible(False)
