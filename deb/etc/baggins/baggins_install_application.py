999418ea1239e25dd5e2e03b4a15e2f04ff259bf
HariZalan <harizalan12@gmail.com>
2025-02-04 06:28:11 +0100

    New script for „installing” web application from CSV-like specification (I have provided a sample as well)

---
+++ deb/etc/baggins/baggins_install_application.py
@@ -0,0 +1,14 @@
+import sys
+from baggins_create_application_lib import *
+try:
+	f=open(sys.argv[1])
+except:
+	print("Error")
+	exit(1)
+csv=f.read()
+csv=csv.split("\n")
+for i in range(len(csv)):
+	csv[i]=csv[i].split("	")
+appname=csv[1][1]
+uri=csv[2][1]
+createApplication(uri,appname,"")
